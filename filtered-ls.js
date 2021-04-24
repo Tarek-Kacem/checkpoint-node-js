367dcdd7103529bbe6baba5cfbd0902544c5b9e4
Tarek-Kacem <79226060+Tarek-Kacem@users.noreply.github.com>
2021-04-24 12:45:31 +0100

    second commit

--- filtered-ls.js
+++ filtered-ls.js
@@ -1,4 +1,6 @@
-const fs = require('fs');
-let buf = fs.readFileSync(process.argv[2])
-console.log(process.argv)
-console.log(buf)
+const fs = require("fs");
+const path = require("path");
+fs.readdir(process.argv[2], (err, files) => {
+  let res = files.filter((el) => path.parse(el).ext === "." + process.argv[3]);
+  res.map(el => console.log(el))
+});
